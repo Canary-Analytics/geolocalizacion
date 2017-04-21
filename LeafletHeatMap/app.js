5ed0e34107b43dbfba0922f2d41eb085beb16832
Samuel Ramos <alu0100815146@ull.edu.es>
2017-04-21 18:50:45 +0100

    Añadiendo server

--- LeafletHeatMap/app.js
+++ LeafletHeatMap/app.js
@@ -11,7 +11,10 @@ app.set('port', process.env.PORT || 3000);
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
-app.use(expressLayouts);
+
+app.get('/', (req,res) => {
+  res.render('index');
+});
 
 app.listen(app.get('port'), () => {
     console.log(`Node app is running at localhost: ${app.get('port')}` );
